566fe0a1e70f06b42f759c1a7be84f5f4bb78452
ouraniargy <78108432+ouraniargy@users.noreply.github.com>
2023-03-13 03:19:09 +0200

    Delete Zoo directory

--- Zoo/elephant.java
+++
@@ -1,7 +0,0 @@
-package com.unipi.talepis;
-
-public class elephant {
-    double weight;
-    int age;
-    String sex;
-}
--- Zoo/giraffe.java
+++
@@ -1,9 +0,0 @@
-package com.unipi.talepis;
-
-public class giraffe {
-    String name;
-    int age;
-    String color;
-    double height;
-    String sex;
-}
